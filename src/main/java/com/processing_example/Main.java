9a90b6306f5658b87fd4bee72023f4da3bd9028e
bikeracer9 <prescottlau@gmail.com>
2024-09-05 12:50:36 -0500

    first commit

---
+++ src/main/java/com/processing_example/Main.java
@@ -0,0 +1,25 @@
+package com.processing_example;
+
+import processing.core.PApplet;
+
+public class Main extends PApplet {
+    public static void main(String[] args) {
+        PApplet.main("com.processing_example.Main");
+        System.out.println("Hello world!");
+    }
+
+    public void settings()
+    {
+        size(500,500);
+    }
+
+    public void setup()
+    {
+        background(0);
+    }
+
+    public void draw()
+    {
+        ellipse(width/2, height/2, width/2, height/2);
+    }
+}
